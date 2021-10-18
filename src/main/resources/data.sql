73b88d121a9a1a22f0a25d6361abed19f6b55510
prakaski <kiran.prakash@sunquestinfo.com>
2021-10-18 18:41:23 +0530

    Added Post Entity and created Many to one relation with User

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,5 @@
 insert into user values(10001,systimestamp,'AB');
 insert into user values(10002,systimestamp,'Jill');
-insert into user values(10003,systimestamp,'Jam')
+insert into user values(10003,systimestamp,'Jam');
+insert into post values(11001, 'My first post', 10001);
+insert into post values(11002, 'My second post', 10001);
